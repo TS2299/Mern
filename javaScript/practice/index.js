673cb1b53adca1fd1f6dfdbe94b5846e39d8c295
TS2299 <tarun22991999@gmail.com>
2023-02-12 12:37:49 +0530

    js class code of 2 and 3 added

---
+++ javaScript/practice/index.js
@@ -0,0 +1,18 @@
+console.log('Hi Tarun This Side');
+
+//1.
+// let a={value:10};
+// let b=a;
+// a.value++;
+// console.log(a.value);
+// console.log(b.value);
+
+//2.
+// let c=9;
+// function inc (c)
+// {
+//     c++;
+//     console.log('inside function c', c);
+// }
+// inc(c);
+// console.log(c);
